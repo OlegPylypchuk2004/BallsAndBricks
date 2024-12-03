d9298027c6d0fe52be3b98f451787f449bcdf26a
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2024-12-03 09:49:12 +0200

    Chaged pickupable ball view

--- Assets/Scripts/PickupableBall.cs
+++ Assets/Scripts/PickupableBall.cs
@@ -1,4 +1,13 @@
+using DG.Tweening;
+using UnityEngine;
+
 public class PickupableBall : PickupableItem
 {
-
+    private void OnEnable()
+    {
+        transform.DORotate(new Vector3(0f, 0f, -360f), 3.75f, RotateMode.FastBeyond360)
+            .SetEase(Ease.Linear)
+            .SetLoops(-1, LoopType.Restart)
+            .SetLink(gameObject);
+    }
 }
