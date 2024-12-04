c5868780363a9999196a2eb7506c45c37796ef48
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2024-12-04 19:44:54 +0200

    Added camera scaler

---
+++ Assets/Scripts/CameraScaler.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CameraScaler : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+
+    private void Awake()
+    {
+        float normalScreenCorrelation = 1920f / 1080f;
+        float normalCameraSize = _camera.orthographicSize;
+        float currentScreenCorrelation = (float)Screen.height / (float)Screen.width;
+        float currentCameraSize = currentScreenCorrelation * normalCameraSize / normalScreenCorrelation;
+
+        _camera.orthographicSize = currentCameraSize;
+    }
+}
