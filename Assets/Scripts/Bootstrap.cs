f105fde2eaf23f2574caed85b9e558230f96a937
OlegPylypchuk2004 <oleg.pylypchuk2004@gmail.com>
2026-03-31 14:25:50 +0300

    Changed bootstrap

--- Assets/Scripts/Bootstrap.cs
+++ Assets/Scripts/Bootstrap.cs
@@ -4,8 +4,18 @@ public class Bootstrap : MonoBehaviour
 {
     private void Start()
     {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 120;
+        double targetFrameRate = 60;
+
+        foreach (Resolution screenResolution in Screen.resolutions)
+        {
+            if (screenResolution.refreshRateRatio.value > 60)
+            {
+                targetFrameRate = screenResolution.refreshRateRatio.value;
+            }
+        }
+
+        Application.targetFrameRate = (int)targetFrameRate;
+
         Time.fixedDeltaTime = 1f / 60f;
 
         SceneChanger.Instance.Load(1);
